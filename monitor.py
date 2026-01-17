68b80d24d6075cce35d3de0b0508e64a55f46c21
Akash <akashbangade341@gmail.com>
2026-01-17 22:59:37 +0530

    Update monitor target URL

--- monitor.py
+++ monitor.py
@@ -1,3 +1,4 @@
 import os
 TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
+URL_TO_MONITOR = "https://www.cricbuzz.com/live-cricket-scores/126603/pc-vs-jsk-28th-match-sa20-2025-26"
